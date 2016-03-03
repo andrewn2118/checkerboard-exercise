4eb65bd3ec5e3bee92d31e82e2b86f0f11edfe70
Andrew Nguyen <andrew.nguyen2118@gmail.com>
2016-03-03 08:59:38 -0800

    adding random color tiles

--- script.js
+++ script.js
@@ -8,13 +8,18 @@ for(var i=0; i < tiles.length; i++){
   tiles[i].style.float = "left";
   tiles[i].style.width = "11.1%";
   tiles[i].style.paddingBottom = "11.1%";
-  if(i % 2 === 0) {
+  /*if(i % 2 === 0) {
     tiles[i].style.backgroundColor = "red";
   } else {
     tiles[i].style.backgroundColor = "black";
-  }
+  }*/
+  tiles[i].style.backgroundColor = `rgb( ${randomColor(255, 0)}, ${randomColor(255, 0)}, ${randomColor(255, 0)})`;
 }
 
 function addElement(location, element){
   document[location].appendChild(element);
+}
+
+function randomColor(max, min){
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
